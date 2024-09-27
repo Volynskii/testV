53ccb95b759b7995c418db382eac4088fce144c6
Volynskii <antonvolynskii@gmail.com>
2024-09-27 14:26:16 +0300

    main, about pages;pinia simple store;

---
+++ pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>about page!!</div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Pinia Counter</h1>
+    <p>Count: {{ counter.count }}</p>
+    <button @click="counter.increment">Increment</button>
+    <button @click="counter.decrement">Decrement</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '~/stores/counter'
+
+const counter = useCounterStore()
+</script>
